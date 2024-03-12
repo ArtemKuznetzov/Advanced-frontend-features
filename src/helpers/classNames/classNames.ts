2e11acc223fe8d1c0a849de7131e975fdc0a8e5c
Artem Kuznetsov <kuznecov.ae@uc-osnovanie.ru>
2024-03-12 19:32:17 +0300

    1.8 add custom classNames, refactor gitignore

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,9 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, mod: Mods, additional: string[]): string {
+    return [cls,
+        ...additional,
+        ...Object.entries(mod).filter(([_cls, value]) => value)
+        .map(([cls, _value]) => cls)]
+        .join(' ')
+}
