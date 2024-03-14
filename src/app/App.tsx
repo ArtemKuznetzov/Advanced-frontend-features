405fe8f245bd98591959aaee35f621ac94d73e52
Artem Kuznetsov <kuznecov.ae@uc-osnovanie.ru>
2024-03-14 18:48:46 +0300

    1.10 Add FSD to project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Routes, Route, Link} from 'react-router-dom'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {THEME, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-
-    const bool = true
 
     return (
         <div className={classNames('app', {}, [theme])}>
@@ -19,7 +16,7 @@ const App = () => {
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} /><Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />} /><Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import React, {FC, useMemo, useState} from 'react';
-import {LOCAL_STORAGE_THEME_KEY, THEME, ThemeContext} from "./ThemeContext";
+import {LOCAL_STORAGE_THEME_KEY, THEME, ThemeContext} from "app/providers/ThemeProvider/lib/ThemeContext";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as THEME || THEME.LIGHT
 
