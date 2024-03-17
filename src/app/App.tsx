3a9865b009f09939c74190ee51200c7054df260c
Artem Kuznetsov <kuznecov.ae@uc-osnovanie.ru>
2024-03-17 14:19:41 +0300

    2.4 Add Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,18 @@ import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
 import AppRouter from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import Sidebar from "widgets/Sidebar";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
-
+            <div className='content-page'>
+                <Sidebar />
+                <AppRouter/>
+            </div>
         </div>
     );
 };
